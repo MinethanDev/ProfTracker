ac39bd06663ea48a2aeeca09363c924e2e7397b8
Antonin <jaimelesfraisesbleues@gmail.com>
2023-09-28 22:31:29 +0200

    new responsive podium in css

--- script.js
+++ script.js
@@ -1,6 +1,42 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const podium = document.getElementById("podium");
+/**
+ * 
+ * @param {string} name Le nom de la matière
+ * @param {int} hours Le nombre d'heures manquées
+ * @param {int} pos La position sur le podium
+ * @returns 
+ */
+function buildPodiumPosition(name, hours, pos) {
+    const podiumTemplate = document.querySelector('[data-podium-template]');
+    // on clone l'élément <template> du HTML pour pouvoir le réutiliser à volonté
+    const podium = podiumTemplate.content.cloneNode(true).children[0]
 
+    // on remplit les valeurs du template cloné
+    podium.querySelector('[data-index]').textContent = pos
+    podium.querySelector('[data-matiere]').querySelector('[data-name]').textContent = name
+    podium.querySelector('[data-matiere]').querySelector('[data-hours]').textContent = hours === 1 ? `${hours} heure manquée` : `${hours} heures manquées`
+
+    switch (pos) {
+        case 1:
+            podium.classList.add('first')
+            break
+        case 2:
+            podium.classList.add('second')
+            break
+        case 3:
+            podium.classList.add('third')
+            break
+        default:
+            podium.classList.remove('position')
+            break
+    }
+
+    return podium
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    const podium = document.getElementById('podium')
+
+    // Chargez le fichier "retards.csv" directement depuis le site
     fetch("retards.csv")
         .then(response => response.text())
         .then(data => {
@@ -18,17 +54,11 @@ document.addEventListener("DOMContentLoaded", function () {
 
             matieres.sort((a, b) => b.Heures - a.Heures);
 
-            const podiumHTML = `
-                <div class="container">
-                <br>
-                    <ol>
-                        <li>${matieres[1].Matiere} <br> ${matieres[1].Heures} heures loupées</li>
-                        <li>${matieres[0].Matiere} <br> ${matieres[0].Heures} heures loupées</li>
-                        <li>${matieres[2].Matiere} <br> ${matieres[2].Heures} heures loupées</li>
-                    </ol>
-                </div>
-            `;
-            podium.innerHTML = podiumHTML;
+            for (let i = 0; i < matieres.length; i++) {
+                // on créé une nouvelle position sur le podium
+                const position = buildPodiumPosition(matieres[i].Matiere, matieres[i].Heures, i+1)
+                podium.appendChild(position)
+            }
         })
         .catch(error => {
             console.error("Une erreur s'est produite lors du chargement du fichier CSV : ", error);
